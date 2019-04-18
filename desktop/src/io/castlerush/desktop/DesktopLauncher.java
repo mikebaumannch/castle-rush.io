85d32c5ef10f2113e18df8beb627cc81d780be55
Sofaschwizermeister 2019 BSCYB! (Mike Baumann) <mike.baumann@ict-campus.net>
2019-04-18 14:21:02 +0200

    Map

---
+++ desktop/src/io/castlerush/desktop/DesktopLauncher.java
@@ -0,0 +1,21 @@
+package io.castlerush.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.castlerush.Castlerush;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		//Options
+		config.title = "CastleRush.io";
+		config.useGL30 = true;
+		config.width = 1280;
+		config.height = 720;
+		config.resizable = false;
+		//config.fullscreen = true;
+		
+		new LwjglApplication(new Castlerush(), config);
+	}
+}
