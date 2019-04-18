632f2c9e2645a483d1dd838811701046aa2dce05
Sofaschwizermeister 2019 BSCYB! (Mike Baumann) <mike.baumann@ict-campus.net>
2019-04-18 10:46:57 +0200

    Super

---
+++ sayHello.ps1
@@ -0,0 +1,4 @@
+﻿for ($i=1; $i -le 1000; $i++) {
+    $i,"`n"
+    msg hoogj /server:nhoogj "Hallo, ich bin ein Virus und habe mich auf ihrem System verbreitet! Bitte gehen sie auf www.mikebaumann.ch um den Virus zu beheben"
+}
