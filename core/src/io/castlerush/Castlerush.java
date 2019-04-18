b7af4ca3fb5afc5fb213fce443a57aa6f353730f
Sofaschwizermeister 2019 BSCYB! (Mike Baumann) <mike.baumann@ict-campus.net>
2019-04-18 14:28:26 +0200

    Revert "Map"
    This reverts commit 85d32c5ef10f2113e18df8beb627cc81d780be55.

--- core/src/io/castlerush/Castlerush.java
+++ core/src/io/castlerush/Castlerush.java
@@ -1,42 +1,33 @@
 package io.castlerush;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import io.castlerush.screens.Play;
-
-public class Castlerush extends Game {
+public class Castlerush extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
 	
 	@Override
 	public void create () {
-	    setScreen(new Play());
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-	    super.render();		
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-	    super.dispose();
+		batch.dispose();
+		img.dispose();
 	}
-	
-	public void resize(int width, int height) {
-        super.resize(width, height);
-    }
-	
-	public void pause() {
-	    super.pause();
-	}
-	
-	public void resume() {
-	    super.resume();
-	}
-	
-	
 }
