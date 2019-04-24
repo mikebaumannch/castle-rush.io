1d84982efd7f8d109af0fb84522a7dfac7c9fbd3
Samuel J. Moor <Samuel.Moor@ict-campus.net>
2019-04-24 10:40:27 +0200

    Player 1.0

--- core/src/io/castlerush/Player.java
+++ core/src/io/castlerush/Player.java
@@ -1,25 +1,59 @@
 package io.castlerush;
 
-public class Player {
-    
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Player extends Sprite {
+
     public String name;
     public int coins;
     public int health;
     public boolean isCastleAlive = true;
-    public float position;
-    
-    private float speed = 1.0F;
-    
-    public Player(String name, int coins, int health, boolean isCastleAlive, float position) {
-        this.name=name;
-        this.coins=coins;
-        this.health=health;
-        this.isCastleAlive=isCastleAlive;
-        this.position=position;
+    public TiledMapTileLayer layer;
+    private Vector2 velocity = new Vector2();
+
+    public Player(String name, Sprite skin, int coins, int health, boolean isCastleAlive,
+            TiledMapTileLayer layer) {
+        super(skin);
+        this.name = name;
+        this.coins = coins;
+        this.health = health;
+        this.isCastleAlive = isCastleAlive;
+        this.layer = layer;
     }
-    
-    void walk() {}
-    void attack() {}
-    void buy( ) {}
+
+    @Override
+    public void draw(Batch spriteBatch) {
+        update(Gdx.graphics.getDeltaTime());
+        super.draw(spriteBatch);
+    }
+
+    private void update(float delta) {
+        /*
+        if (!checkCollision) {
+            setX(getX() + velocity.x * delta);
+        }
+
+        if (!checkCollision) {
+            setY(getY() + velocity.y * delta);
+        }*/
+
+    }
+
+    void walk() {
+    }
+
+    void attack() {
+    }
+
+    void buy() {
+    }
 
 }
--- core/src/io/castlerush/screens/Play.java
+++ core/src/io/castlerush/screens/Play.java
@@ -6,42 +6,64 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+
+import io.castlerush.Player;
 
 public class Play implements Screen {
+
+    // PLayer erstellen
+    private Player player;
 
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
-    private KeyListener keyListener = new KeyListener(camera);
-    
+    private KeyListener keyListener;
     @Override
     public void show() {
         map = new TmxMapLoader().load("maps/maps.tmx");
-        
+
         renderer = new OrthogonalTiledMapRenderer(map);
-        
+
         camera = new OrthographicCamera();
         camera.zoom = 1 / 3f;
-             
-        
+
+        player = new Player("Markus", (new Sprite(new Texture("img/player.png"))), 100, 100, true,
+                (TiledMapTileLayer) map.getLayers().get(0));
+        keyListener = new KeyListener(player, map);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(100 / 255f, 155 / 255f, 255 / 255f, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
+
         keyListener.handleInput();
-        
+
+        camera.position.set(player.getX() + player.getWidth() / 2,
+                player.getY() + player.getHeight() / 2, 0);
         camera.update();
-        
+
         renderer.setView(camera);
         renderer.render();
+
+        player.setSize((map.getProperties().get("tilewidth", Integer.class)) * 2,
+                map.getProperties().get("tileheight", Integer.class) * 2);
+
+        renderer.getBatch().begin();
+        player.draw(renderer.getBatch());
+        renderer.getBatch().end();
+
     }
 
     @Override
@@ -75,5 +97,5 @@ public class Play implements Screen {
         renderer.dispose();
 
     }
-   
+
 }
