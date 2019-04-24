5c1fe4c5bb2de7a62b1134534eec7b9817972547
Sofaschwizermeister 2019 BSCYB! (Mike Baumann) <mike.baumann@ict-campus.net>
2019-04-24 11:28:47 +0200

    Working map with player and random spawnpoint

--- core/src/io/castlerush/screens/KeyListener.java
+++ core/src/io/castlerush/screens/KeyListener.java
@@ -3,35 +3,70 @@ package io.castlerush.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.objects.TiledMapTileMapObject;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+
+import io.castlerush.Player;
 
 public class KeyListener implements InputProcessor {
-    
-    private OrthographicCamera camera;
+
+    private Player player;
+    private TiledMap map;
     public boolean keyPressed = false;
-    
-    public KeyListener(OrthographicCamera camera){
-        this.camera = camera;
-        Gdx.input.setInputProcessor(this);       
+
+    public KeyListener(Player player, TiledMap map) {
+        this.player = player;
+        this.map = map;
+        Gdx.input.setInputProcessor(this);
     }
-    
+
+    public boolean checkCollision() {
+
+        TiledMapTileLayer collisionObjectLayer = (TiledMapTileLayer)map.getLayers().get("Water");
+        MapObjects objects = collisionObjectLayer.getObjects();
+
+        // there are several other types, Rectangle is probably the most common one
+        for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
+            
+            System.out.println(rectangleObject.getName());
+
+            Rectangle rectangle = rectangleObject.getRectangle();
+            if (Intersector.overlaps(rectangle, player.getBoundingRectangle())) {
+                // collision happened
+                
+                return true;
+            }else {
+                return false;
+            }
+        }
+        return false;
+    }
+
     void handleInput() {
-        if(keyPressed) {
-            if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-                camera.translate(-1,0);
-            }
-            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-                camera.translate(1,0);
-            }
-            if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
-                camera.translate(0,-1);
-            }
-            if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-                camera.translate(0,1);
+        if (keyPressed) {
+            if (!checkCollision()) {
+                if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+                    player.translate(-1, 0);
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+                    player.translate(1, 0);
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+                    player.translate(0, 1);
+                }
+                if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+                    player.translate(0, -1);
+                }
             }
         }
     }
-    
+
     @Override
     public boolean keyUp(int keycode) {
         keyPressed = false;
@@ -49,7 +84,7 @@ public class KeyListener implements InputProcessor {
         // TODO Auto-generated method stub
         return false;
     }
-    
+
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         // TODO Auto-generated method stub
